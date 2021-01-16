c71dfeebd0e9c07fd9c01809fde9a3ca4d3604a7
Kevin Truong <tig3r86@gmail.com>
2021-01-16 21:14:37 +1100

    add eslint & prettier setup

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const a = 'asdfa'
   return (
     <div className={styles.container}>
       Hello world
+      <h1>{a}</h1>
     </div>
-  );
+  )
 }
